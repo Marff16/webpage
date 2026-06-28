2b990a2facc430464d8e20a8d59e574361aecd9d
marff16 <marvin.guelhan@proton.me>
2026-06-28 13:44:04 +0200

    update read time

--- assets/js/posts-data.js
+++ assets/js/posts-data.js
@@ -5,7 +5,7 @@
       title: 'Starting',
       summary: 'A short first note about what this page is for, why I want to write here, and what kind of posts I want to collect over time.',
       date: '2026-05-26',
-      readTime: '2 min read',
+      readTime: '1 min read',
       href: {
         en: 'pages/en/posts/starting/',
         de: 'pages/en/posts/starting/'
